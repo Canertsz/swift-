a7011dc65ad2ffa7b7787ce61d70ce42dcc3aeea
Caner Tüysüz <canertuysuz@hotmail.com>
2024-12-04 13:44:51 +0300

    project initialization
    project initialization

---
+++ MarvelApp/MarvelAppTests/MarvelAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAppTests.swift
+//  MarvelAppTests
+//
+//  Created by Caner Tüysüz on 4.12.2024.
+//
+
+import Testing
+@testable import MarvelApp
+
+struct MarvelAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
